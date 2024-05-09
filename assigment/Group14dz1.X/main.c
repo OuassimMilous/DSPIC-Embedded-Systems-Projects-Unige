749f5e83942f19f8a24fa860d1659aeefafaff8e
Ouassim Milous <59444113+OuassimMilous@users.noreply.github.com>
2024-05-10 00:40:40 +0200

    revised

---
+++ assigment/Group14dz1.X/main.c
@@ -0,0 +1,97 @@
+#include "xc.h"
+#include"tools.h"
+#include <stdio.h>
+#include <math.h>
+
+//declaring variables
+char msg[BUFFER_SIZE];
+int counter10ms = 0;
+int angle;
+int magX = 0, magY = 0, magZ = 0;
+CircularBuffer cb;
+
+// an interrupt triggered when the UART is ready to print
+void __attribute__((__interrupt__, __auto_psv__))_U1TXInterrupt() {
+    //resetting the interrupt flag
+    IFS0bits.U1TXIF = 0;
+    //printing the queued data
+    int data = dequeue(&cb);
+    if (data != -1) {
+        print_UART1(data);
+    }
+}
+
+//simulating an algorithm that takes 7ms to execute
+void algorithm(){
+    tmr_wait_ms(TIMER2, 7);
+}
+
+// the main function
+int main(void) {
+    //clearing the analog registers
+    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
+    
+    //initializing the circular buffer that will be queuing the data we want to print
+    initCircularBuffer(&cb);
+    
+    //initializing SPI to communicate with and set up the magnetometer
+    init_SPI1();
+    setup_mag();
+    
+    //initiating UART to print results
+    init_UART1();
+    
+    //enabling the TX interrupt, it is triggered when UART is available for printing
+    U1STAbits.UTXISEL0 = 0b0;
+    U1STAbits.UTXISEL1 = 0b0;
+    IEC0bits.U1TXIE = 1;
+    
+    //setting up the period to work at a 100Hz rate
+    tmr_setup_period(TIMER1, 10);
+    while (1) {
+        //simulating an algorithm
+        algorithm();
+
+        // the code inside the if condition works at the rate of 25Hz
+        if (counter10ms == 4) {
+            // getting the X,Y, and Z data from the magnetometer and calculates the average of every 5 values
+            magX = mag_get_x() / 5 + magX;
+            magY = mag_get_y() / 5 + magY;
+            magZ = mag_get_z() / 5 + magZ;
+        }
+        
+        // the code inside the if condition works at the rate of 5Hz
+        if (counter10ms == 20) {
+            // we calculate the angle to the magnetic north then we prepare the String that needs to be printed
+            sprintf(msg, "$MAG,%d,%d,%d*, $YAW,%f* ", magX, magY, magZ, atan2(magY,magX) * 180.0 / M_PI);
+            
+            // we queue all the characters of the string to be printed to the UART
+            int c = 0;
+            do {
+                enqueue(&cb, msg[c]);
+                c++;
+            } while (msg[c ] != '\0');
+            
+            // separating prints by a space
+            enqueue(&cb, ' ');
+            
+            // we force the first print out to make sure our interrupts will be triggered correctly
+            print_UART1(dequeue(&cb));
+            
+            // resetting the data to be recollected again
+            magX = 0;
+            magY = 0;
+            magZ = 0;
+            
+            //we reset the counter to restart
+            counter10ms = 0;
+        }
+        // we ensure the 100Hz rate in case we finish early
+        tmr_wait_period(TIMER1);
+        
+        //a counter to keep everything synchronized
+        counter10ms++;
+    }
+ 
+    return 0;
+}
---
+++ assigment/Group14dz1.X/tools.h
@@ -0,0 +1,62 @@
+
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+#include <xc.h> 
+
+//defining constants
+#define TIMER1 1
+#define TIMER2 2
+#define CS LATDbits.LATD6
+#define BUFFER_SIZE 50
+
+//defining registers
+
+#define MAGCHIPID 0x40
+#define MAGSLEEPMODE 0x4B
+#define MAGOPMODE 0x4C
+
+#define MAGXLSB 0x42
+#define MAGXMSB 0x43
+
+#define MAGYLSB 0x44
+#define MAGYMSB 0x45
+
+#define MAGZLSB 0x46
+#define MAGZMSB 0x47
+
+#define MAGHALLLSB 0x48
+#define MAGHALLMSB 0x49
+
+
+//defining structs
+typedef struct {
+    char buffer[BUFFER_SIZE];
+    int head;
+    int tail;
+    int count;
+} CircularBuffer;
+
+
+
+// Function prototypes
+void tmr_setup_period(int timer, int ms);
+int tmr_wait_period(int timer);
+void tmr_wait_ms(int timer, int ms);
+void init_UART1();
+void print_UART1(unsigned char msg);
+void print_buffer_UART1(char buffer[]);
+void init_SPI1();
+void write_SPI1(unsigned char addr, unsigned char msg);
+unsigned char read_SPI1(unsigned char addr);
+void setup_mag();
+int mag_get_x();
+int mag_get_y();
+int mag_get_z();
+void initCircularBuffer(CircularBuffer *cb);
+int isEmpty(CircularBuffer *cb);
+int isFull(CircularBuffer *cb);
+void enqueue(CircularBuffer *cb, char value);
+char dequeue(CircularBuffer *cb);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
